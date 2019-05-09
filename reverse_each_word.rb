37a20ddad98e89c391e8d3bf9d81ed04074c6ae9
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-05-09 20:37:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(array)
-  array.n=array.split(" ")
+def reverse_each_word(string)
+  array_n=string.split(" ")
   another_array=array_n.each {|i| i.reverse!}
   return another_array.join(" ")
 end
